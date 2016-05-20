b7ceb9f246eda0512ced1a5974bd1e3011dd4873
Luis Herranz <luisherranz@gmail.com>
2016-05-20 17:22:23 +0200

    Redirect to sites after creating one.

--- worona-dashboard-client/extensions/sites/sagas/createSite.js
+++ worona-dashboard-client/extensions/sites/sagas/createSite.js
@@ -4,6 +4,7 @@ import { CREATE_SITE_REQUESTED } from '../actiontypes';
 import { CREATING_SITE } from '../messages';
 import { createSiteSucceed, createSiteFailed, createSiteStatusChanged } from '../actions';
 import { createSite } from '../libs';
+import { browserHistory } from '../dependencies';
 
 export function* createSiteSaga(action) {
   const { name, url, _id } = action;
@@ -11,6 +12,7 @@ export function* createSiteSaga(action) {
     yield put(createSiteStatusChanged(CREATING_SITE));
     const siteId = yield call(createSite, { name, url, _id });
     yield put(createSiteSucceed(siteId));
+    yield call(browserHistory.push, '/');
   } catch (error) {
     yield put(createSiteFailed(error));
   }
