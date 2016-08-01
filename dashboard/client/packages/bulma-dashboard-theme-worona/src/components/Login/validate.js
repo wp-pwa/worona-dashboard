7eaefe53752bfd2af06f699ab51035dba7236b51
Luis Herranz <luisherranz@gmail.com>
2016-08-01 16:46:45 +0200

    Update to Redux-Forms 6 (rc3)

--- dashboard/client/packages/bulma-dashboard-theme-worona/src/components/Login/validate.js
+++ dashboard/client/packages/bulma-dashboard-theme-worona/src/components/Login/validate.js
@@ -13,7 +13,7 @@ export const validate = values => {
   const errors = {};
   if (!values.email) {
     errors.email = messages.required;
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,8}$/i.test(values.email)) {
     errors.email = messages.invalidEmail;
   }
   if (!values.password) {
--- dashboard/client/packages/bulma-dashboard-theme-worona/src/elements/Input/index.jsx
+++ dashboard/client/packages/bulma-dashboard-theme-worona/src/elements/Input/index.jsx
@@ -2,31 +2,24 @@ import React from 'react';
 import cx from 'classnames';
 import styles from './style.css';
 
-const Input = (props) => {
-  const { type = 'text', label, placeholder, icon, value = '', color, size, help } = props;
-  return (
-    <div className={styles.input}>
-      {label ? <label className="label">{label}</label> : null}
-      <p className={cx('control', icon && 'has-icon')}>
-        <input className={cx('input', color && `is-${color}`, size && `is-${size}`)}
-          type={type} placeholder={placeholder} value={value} {...props}
-        />
-        {icon ? <i className={`fa fa-${icon}`}></i> : null}
-        {help ? <span className={`help is-${color}`}>{help}</span> : null}
-      </p>
-    </div>
-  );
-};
+const Input = ({ input, touched, error, size, label }) =>
+  <div className={styles.input}>
+    {label && <label className="label">{label}</label>}
+    <p className={cx('control', input.icon && 'has-icon')}>
+      <input className={cx('input', error && touched && 'is-danger', size && `is-${size}`)}
+        {...input}
+      />
+    {input.icon && <i className={`fa fa-${input.icon}`}></i>}
+      {touched && error && <span className="help is-danger">{error}</span>}
+    </p>
+  </div>;
 
 Input.propTypes = {
-  type: React.PropTypes.string,
+  input: React.PropTypes.object,
   label: React.PropTypes.string,
-  placeholder: React.PropTypes.string,
-  icon: React.PropTypes.string,
-  value: React.PropTypes.string,
-  color: React.PropTypes.string,
+  error: React.PropTypes.string,
+  touched: React.PropTypes.bool,
   size: React.PropTypes.string,
-  help: React.PropTypes.string,
 };
 
 export default Input;
