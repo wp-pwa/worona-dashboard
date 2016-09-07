7912acfba6fd43d66ffcae01e62541fe1aa14809
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2016-09-07 17:22:40 +0200

    Removed test not longer needed

--- dashboard/client/packages/bulma-dashboard-theme-worona/src/tests/reducers.js
+++ dashboard/client/packages/bulma-dashboard-theme-worona/src/tests/reducers.js
@@ -1,7 +1,6 @@
 import test from 'ava';
 import { mock } from 'worona-deps';
 import { showingMobileMenu } from '../reducers/header';
-import { validationFailed } from '../reducers/forms';
 import * as actions from '../actions';
 import * as deps from '../dependencies';
 
@@ -13,10 +12,4 @@ test('header.showingMobileMenu', t => {
   t.true(showingMobileMenu(false, actions.toggleMobileMenu()));
   t.false(showingMobileMenu(true, actions.toggleMobileMenu()));
   t.false(showingMobileMenu(true, deps.actions.logoutSucceed()));
-});
-
-test('forms.register.validationFailed', t => {
-  t.false(validationFailed(undefined, {}));
-  t.true(validationFailed(false, { type: 'redux-form/SUBMIT_FAILED', form: 'register' }));
-  t.false(validationFailed(true, { type: 'any' }));
 });
