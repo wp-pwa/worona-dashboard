68dc88206798ea452dad1415cd748a64871d5635
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2016-09-30 11:42:00 +0200

    stringifyError in every error of the dashboard
    excepty for categoryIndex which is waiting for @luisherranz to refactor

--- client/packages/connection-dashboard-extension-worona/src/actions/index.js
+++ client/packages/connection-dashboard-extension-worona/src/actions/index.js
@@ -1,7 +1,9 @@
 import * as types from '../types';
+import stringifyError from 'stringify-error-message';
 
 export const connectionStarted = () => ({ type: types.CONNECTION_STARTED });
 export const connectionRequested = () => ({ type: types.CONNECTION_REQUESTED });
 export const connectionSucceed = () => ({ type: types.CONNECTION_SUCCEED });
-export const connectionFailed = error => ({ type: types.CONNECTION_FAILED, error });
+export const connectionFailed = errorObj =>
+({ type: types.CONNECTION_FAILED, error: stringifyError(errorObj) });
 export const disconnected = error => ({ type: types.DISCONNECTED, error });
