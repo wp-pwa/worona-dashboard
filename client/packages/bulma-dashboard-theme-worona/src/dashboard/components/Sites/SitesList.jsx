e19ebbe57bf1e698144f8684b6263a7762b2e285
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2016-09-30 17:45:51 +0200

    Added SiteList content when there's no any site. Fixes #34

--- client/packages/bulma-dashboard-theme-worona/src/dashboard/components/Sites/SitesList.jsx
+++ client/packages/bulma-dashboard-theme-worona/src/dashboard/components/Sites/SitesList.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
+import { Link } from 'react-router';
+
 import Site from './Site';
 import * as deps from '../../dependencies';
 
-const SitesList = ({ sites }) => (
-  <div className="columns is-multiline">
-    {sites.map(site => (
-      <Site {...site} date={site.modifiedAt.$date} key={site.id} />
-    ))}
-  </div>
-);
+const SitesList = ({ sites }) => {
+  if (sites.length === 0) {
+    return (
+      <section className="hero is-medium">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <h1 className="title">
+              No sites found
+            </h1>
+            <h2 className="subtitle">
+              <Link to="/add-site">Add a WordPress site</Link> to start using Worona
+            </h2>
+          </div>
+        </div>
+      </section>
+    );
+  }
+  return (
+    <div className="columns is-multiline">
+      {sites.map(site => (
+        <Site {...site} date={site.modifiedAt.$date} key={site.id} />
+      ))}
+    </div>
+  );
+};
+
 SitesList.propTypes = {
   sites: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
-  t: React.PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
