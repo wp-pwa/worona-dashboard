13a5e9b9cf03bd7c37d13a2e66e6f15a3de13287
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2016-10-07 12:22:35 +0200

    Fix tests for sites-dashboard-extension-worona package

--- client/packages/sites-dashboard-extension-worona/src/dashboard/tests/libs.js
+++ client/packages/sites-dashboard-extension-worona/src/dashboard/tests/libs.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 import sinon from 'sinon';
-import { createSite } from '../libs';
+import { createSite, deleteSite } from '../libs';
 
 test('createSite with id', t => {
   const caller = sinon.stub();
@@ -14,4 +14,11 @@ test('createSite without id', t => {
   const action = { name: 'name', url: 'url', _id: undefined };
   createSite(Object.assign({}, action, { caller }));
   t.true(caller.calledWith('createSite', action));
+});
+
+test('deleteSite', t => {
+  const caller = sinon.stub();
+  const action = { _id: '1234' };
+  deleteSite(Object.assign({}, action, { caller }));
+  t.true(caller.calledWith('deleteSite', action));
 });
--- client/packages/sites-dashboard-extension-worona/src/dashboard/tests/reducers.js
+++ client/packages/sites-dashboard-extension-worona/src/dashboard/tests/reducers.js
@@ -1,6 +1,11 @@
 import test from 'ava';
+import deepFreeze from 'deep-freeze';
+import { mock } from 'worona-deps';
 import * as reducers from '../reducers';
 import * as actions from '../actions';
+import * as deps from '../dependencies';
+
+mock(deps);
 
 test('isCreatingSite', t => {
   t.false(reducers.isCreatingSite(undefined, {}));
@@ -11,6 +16,7 @@ test('isCreatingSite', t => {
 
 test('createSiteStatus', t => {
   const status = {};
+  deepFreeze(status);
   t.false(reducers.createSiteStatus(undefined, {}));
   t.is(reducers.createSiteStatus(false, actions.createSiteStatusChanged(status)), status);
   t.false(reducers.createSiteStatus(status, actions.createSiteSucceed()));
@@ -18,9 +24,35 @@ test('createSiteStatus', t => {
 });
 
 test('createSiteError', t => {
-  const error = {};
+  const msg = 'Some Sites Error';
+  const error = new Error(msg);
+  deepFreeze(error);
   t.false(reducers.createSiteError(undefined, {}));
-  t.is(reducers.createSiteError(false, actions.createSiteFailed(error)), error);
+  t.is(reducers.createSiteError(false, actions.createSiteFailed(error)), msg);
   t.false(reducers.createSiteError(error, actions.createSiteRequested()));
   t.false(reducers.createSiteError(error, actions.createSiteSucceed()));
+});
+
+test('newSiteInfo', t => {
+  t.deepEqual(reducers.newSiteInfo(undefined, {}), {});
+  t.deepEqual(reducers.newSiteInfo(undefined, actions.createSiteSucceed), {});
+
+  const mockAction = { type: deps.types.ROUTER_DID_CHANGE };
+  const noQuery = Object.assign({ type: deps.types.ROUTER_DID_CHANGE, payload: { location: { pathname: '/', query: {} } } }, mockAction);
+  deepFreeze(noQuery);
+  t.deepEqual(reducers.newSiteInfo(undefined, noQuery), {});
+  const siteName = 'foo';
+  const siteNameQuery = Object.assign({ type: deps.types.ROUTER_DID_CHANGE, payload: { location: { pathname: '/', query: { siteName } } } }, mockAction);
+  deepFreeze(siteNameQuery);
+  t.deepEqual(reducers.newSiteInfo(undefined, siteNameQuery), { siteName });
+  const siteId = '123';
+  const siteNameAndIdQuery = Object.assign({ type: deps.types.ROUTER_DID_CHANGE, payload: { location: { pathname: '/', query: { siteName, siteId } } } }, mockAction);
+  deepFreeze(siteNameAndIdQuery);
+  t.deepEqual(reducers.newSiteInfo(undefined, siteNameAndIdQuery), { siteName, siteId });
+  const siteURL = 'https://foo.blog';
+  const siteNameURLAndIdQuery = Object.assign({ type: deps.types.ROUTER_DID_CHANGE, payload: { location: { pathname: '/', query: { siteName, siteId, siteURL } } } }, mockAction);
+  const newSiteInfoState = { siteName, siteId, siteURL };
+  deepFreeze(newSiteInfoState);
+  deepFreeze(siteNameURLAndIdQuery);
+  t.deepEqual(reducers.newSiteInfo(undefined, siteNameURLAndIdQuery), newSiteInfoState);
 });
