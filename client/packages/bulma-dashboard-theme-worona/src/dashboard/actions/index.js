d22b4ff2ee6a61078922e29f85de9a8c3d5fe3e0
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2016-11-02 17:58:46 +0100

    Working ImageUploader component

--- client/packages/bulma-dashboard-theme-worona/src/dashboard/actions/index.js
+++ client/packages/bulma-dashboard-theme-worona/src/dashboard/actions/index.js
@@ -4,3 +4,5 @@ export const toggleMobileMenu = () => ({ type: types.TOGGLE_MOBILE_MENU });
 export const closeMobileMenu = () => ({ type: types.CLOSE_MOBILE_MENU });
 export const toggleTermsAndConditions = () => ({ type: types.TOGGLE_TERMS_AND_CONDITIONS });
 export const closeTermsAndConditions = () => ({ type: types.CLOSE_TERMS_AND_CONDITIONS });
+export const uploadSucceed = (fileId) => ({ type: types.UPLOAD_SUCCEED, fileId });
+export const uploadError = (status) => ({ type: types.UPLOAD_ERROR, status });
--- client/packages/bulma-dashboard-theme-worona/src/dashboard/components/Publish/index.jsx
+++ client/packages/bulma-dashboard-theme-worona/src/dashboard/components/Publish/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import DropzoneS3Uploader from 'react-dropzone-s3-uploader';
 
+import * as actions from '../../actions';
+import * as deps from '../../deps';
 import Header from '../Header';
 import Hero from '../elements/Hero';
 import Body from '../Body';
@@ -8,26 +11,71 @@ import Footer from '../Footer';
 import FooterLinks from '../Footer/FooterLinks';
 import Main from '../Main';
 
-function ImageUploader() {
-  const style = {
-    height: 200,
-    border: 'dashed 2px #999',
-    borderRadius: 5,
-    position: 'relative',
-    cursor: 'pointer',
-  };
-  const uploaderProps = {
-    style,
-    maxFileSize: 1024 * 1024 * 50,
-    s3Url: 'https://my-bucket.s3.amazonaws.com/',
-    signingUrlQueryParams: { uploadType: 'avatar' },
-    filename: 'app_icon',
-  };
-  debugger;
-  return (
-    <DropzoneS3Uploader onFinish={this.handleFinishedUpload} {...uploaderProps} />
-  );
+class ImageUploaderClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { progress: 0 };
+  }
+
+  render() {
+    const style = {
+      height: 200,
+      border: 'dashed 2px #999',
+      borderRadius: 5,
+      position: 'relative',
+      cursor: 'pointer',
+      width: 200,
+    };
+    const uploaderProps = {
+      style,
+      server: 'http://localhost:4500',
+      signingUrl: '/api/v1/s3/sign',
+      signingUrlQueryParams: { siteId: this.props.siteId, imgType: 'icon' },
+      accept: 'image/*',
+      multiple: false,
+      signingUrlHeaders: { additional: 'Access-Control-Allow-Origin' },
+      progressComponent: () => (
+        <progress className="progress is-primary" value={this.state.progress} max="100" />
+      ),
+    };
+
+    const onUploadProgress = (percent) => {
+      this.setState({ progress: percent });
+    };
+
+    return (
+      <div className="container">
+        <DropzoneS3Uploader
+          onFinish={this.props.handleFinishedUpload}
+          onError={this.props.handleUploadError}
+          onProgress={onUploadProgress}
+          {...uploaderProps}
+        >
+          <div>
+            <p>Drop your app icon here.</p>
+          </div>
+        </DropzoneS3Uploader>
+      </div>
+    );
+  }
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  handleFinishedUpload: (signResult) => dispatch(actions.uploadSucceed(signResult.filename)),
+  handleUploadError: (status) => dispatch(actions.uploadError(status)),
+});
+
+const mapStateToProps = (state) => ({
+  siteId: deps.selectors.getSelectedSiteId(state),
+});
+
+ImageUploaderClass.propTypes = {
+  handleFinishedUpload: React.PropTypes.func.isRequired,
+  handleUploadError: React.PropTypes.func.isRequired,
+  siteId: React.PropTypes.string,
+};
+
+const ImageUploader = connect(mapStateToProps, mapDispatchToProps)(ImageUploaderClass);
 
 const Publish = () => (
   <Body>
