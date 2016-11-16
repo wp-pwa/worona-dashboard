644601556954de54dd935c9b84b82afc265dc7ad
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2016-11-16 11:35:06 +0100

    Adapted profile selectors to new-collection-subscription-system branch changes.

--- client/packages/profile-dashboard-extension-worona/src/dashboard/selectors/index.js
+++ client/packages/profile-dashboard-extension-worona/src/dashboard/selectors/index.js
@@ -1,5 +1,5 @@
 export const getNameAndEmail = state => {
-  const email = state.profile.userInfo.emails[0].address;
-  const name = state.profile.userInfo.profile.name;
+  const email = state.profile.user.emails[0].address;
+  const name = state.profile.user.profile.name;
   return { email, name };
 };
