bfca9fd13d99973827a3487b42afa62299c33cfb
Luis Herranz <luisherranz@gmail.com>
2016-11-16 13:53:54 +0100

    Add devPackages to AsideMenu

--- client/development/entry.js
+++ client/development/entry.js
@@ -1,7 +1,23 @@
 import 'worona-polyfills';
 import { packageDevelopment } from 'worona-deps';
 import pkg from './test-development-package-worona/src/dashboard';
+import pkgJson from './test-development-package-worona/package.json';
 
-packageDevelopment(pkg);
+packageDevelopment({
+  name: pkgJson.name,
+  namespace: pkgJson.worona.namespace,
+  woronaInfo: {
+    name: pkgJson.name,
+    id: pkgJson.name,
+    namespace: pkgJson.worona.namespace,
+    niceName: pkgJson.worona.niceName,
+    type: pkgJson.worona.type,
+    menu: {
+      category: 'Development',
+      order: 10,
+    },
+  },
+  ...pkg,
+});
 
 console.log('test-development-package-worona loaded!');
--- client/packages/settings-dashboard-extension-worona/src/dashboard/selectors/index.js
+++ client/packages/settings-dashboard-extension-worona/src/dashboard/selectors/index.js
@@ -7,26 +7,26 @@ export const getSettingsLiveCollection = state => state.settings.collections.liv
 export const getSettingsLiveIsReady = state => state.settings.collections.live.isReady;
 export const getSettingsPreviewCollection = state => state.settings.collections.preview.collection;
 export const getSettingsPreviewIsReady = state => state.settings.collections.preview.isReady;
-export const getSettingsPackageCollection = state => state.settings.collections.packages.collection;
-export const getSettingsPackageIsReady = state => state.settings.collections.packages.isReady;
+export const getPackageCollection = state => state.settings.collections.packages.collection;
+export const getPackageIsReady = state => state.settings.collections.packages.isReady;
+export const getDevPackageCollection = state => state.settings.collections.devPackages.collection;
 
 export const getCategories = createSelector(
   getSettingsLiveCollection,
-  getSettingsPackageCollection,
-  (settings, packages, development) => {
-    const normal = flow(
+  getPackageCollection,
+  getDevPackageCollection,
+  (settings, packages, devPackages) => {
+    const pkgsWithSettings = flow(
       map(item => packages[findIndex(packages, pkg => pkg.name === item.woronaInfo.name)]),
       filter(item => typeof item !== 'undefined'),
       sortBy(item => item.menu.order),
       groupBy(item => item.menu.category)
     )(settings);
-    return { ...normal, Development: [{
-      niceName: 'Dev',
-      name: 'test-development',
-      namespace: 'development',
-      id: 1,
-      type: 'extension',
-    }] };
+    const pkgsFromDev = flow(
+      sortBy(item => item.menu.order),
+      groupBy(item => item.menu.category)
+    )(devPackages);
+    return { ...pkgsWithSettings, ...pkgsFromDev };
   }
 );
 
