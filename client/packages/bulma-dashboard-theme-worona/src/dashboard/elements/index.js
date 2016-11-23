e25bc9dd8b24e8ff18c66ee37f9715f0df5dba24
Luis Herranz <luisherranz@gmail.com>
2016-11-23 12:12:26 +0100

    Remove publish package on bulma and add elements index.

---
+++ client/packages/bulma-dashboard-theme-worona/src/dashboard/elements/index.js
@@ -0,0 +1,6 @@
+export { default as Button } from './Button';
+export { default as Checkbox } from './Checkbox';
+export { default as EditSiteLink } from './EditSiteLink';
+export { default as Hero } from './Hero';
+export { default as Icon } from './Icon';
+export { default as Input } from './Input';
