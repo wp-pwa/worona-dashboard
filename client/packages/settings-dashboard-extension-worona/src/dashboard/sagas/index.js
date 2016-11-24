7eec5a80312b05d75ccbf76ed0fffed10dec774c
Luis Herranz <luisherranz@gmail.com>
2016-11-24 16:12:38 +0100

    Fix settings subscription.

--- client/packages/settings-dashboard-extension-worona/src/dashboard/sagas/index.js
+++ client/packages/settings-dashboard-extension-worona/src/dashboard/sagas/index.js
@@ -6,7 +6,7 @@ const env = isDev ? 'dev' : 'prod';
 
 export default function* settingsagas() {
   yield [
-    fork(deps.sagaCreators.collectionWatcherCreator('sites-live')),
+    fork(deps.sagaCreators.collectionWatcherCreator('settings-live')),
     fork(deps.sagaCreators.subscriptionWatcherCreator('settings-live')),
     fork(deps.sagaCreators.collectionWatcherCreator('packages')),
     fork(deps.sagaCreators.subscriptionWatcherCreator('packages', env)),
