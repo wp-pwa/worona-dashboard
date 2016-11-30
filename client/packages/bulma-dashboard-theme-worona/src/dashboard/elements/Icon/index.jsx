fe58de3f190aa69972239230fc455e6c6937e899
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2016-11-30 12:47:13 +0100

    Added color prop to element Icon

--- client/packages/bulma-dashboard-theme-worona/src/dashboard/elements/Icon/index.jsx
+++ client/packages/bulma-dashboard-theme-worona/src/dashboard/elements/Icon/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Icon = ({ code, small }) => (
-  <span className={`icon ${(small ? 'is-small' : '')}`}>
+const Icon = ({ code, small, color }) => (
+  <span className={`icon ${(small ? 'is-small' : '')}`} style={{ color }}>
     <i className={`fa fa-${code}`} />
   </span>
 );
 Icon.propTypes = {
   code: React.PropTypes.string.isRequired,
   small: React.PropTypes.bool,
+  color: React.PropTypes.string,
 };
 
 export default Icon;
