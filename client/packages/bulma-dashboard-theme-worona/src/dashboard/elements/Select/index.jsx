a180bdb7b4f98e4169766b6d555040b9e7fa77c6
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2016-12-14 17:26:22 +0100

    Better implementation for Select element

--- client/packages/bulma-dashboard-theme-worona/src/dashboard/elements/Select/index.jsx
+++ client/packages/bulma-dashboard-theme-worona/src/dashboard/elements/Select/index.jsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import cx from 'classnames';
 
-const Select = ({ size, options, selected }) => {
-  let generateOption;
-  if (!selected) {
-    generateOption = (option) => <option value={option}>{option}</option>;
-  } else {
-    generateOption = (option) => {
-      if (selected === option) {
-        return (<option value={option} selected>{option}</option>);
-      }
-      return (<option value={option}>{option}</option>);
-    };
-  }
-  return (
-    <span className={cx('select', size && `is-${size}`)}>
-      <select>
-        {options.map(generateOption)}
-      </select>
-    </span>
+const Select = ({ size, options, selected }) => (
+  <span className={cx('select', size && `is-${size}`)}>
+    <select defaultValue={selected}>
+      {options.map((option, index) => <option value={option} key={index}>{option}</option>)}
+    </select>
+  </span>
  );
-};
 
 Select.propTypes = {
   size: React.PropTypes.string,
