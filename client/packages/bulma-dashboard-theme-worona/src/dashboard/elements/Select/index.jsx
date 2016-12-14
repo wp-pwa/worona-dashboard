923bdb56a5dc1d5a732ba96fdcf4806e0808b6fe
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2016-12-14 17:49:13 +0100

    Improved Select element and eslinted Input

--- client/packages/bulma-dashboard-theme-worona/src/dashboard/elements/Select/index.jsx
+++ client/packages/bulma-dashboard-theme-worona/src/dashboard/elements/Select/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import cx from 'classnames';
 
-const Select = ({ size, options, selected }) => (
-  <span className={cx('select', size && `is-${size}`)}>
-    <select defaultValue={selected}>
-      {options.map((option, index) => <option value={option} key={index}>{option}</option>)}
-    </select>
-  </span>
+const Select = ({ name, size, options, selected, label }) => (
+  <p className="control">
+    {label && <label className="label" htmlFor={name}>{label}</label>}
+    <span className={cx('select', size && `is-${size}`)}>
+      <select defaultValue={selected}>
+        {options.map((option, index) => <option value={option} key={index}>{option}</option>)}
+      </select>
+    </span>
+  </p>
  );
 
 Select.propTypes = {
+  name: React.PropTypes.string,
+  label: React.PropTypes.string,
   size: React.PropTypes.string,
   options: React.PropTypes.arrayOf(React.PropTypes.number),
   selected: React.PropTypes.number,
