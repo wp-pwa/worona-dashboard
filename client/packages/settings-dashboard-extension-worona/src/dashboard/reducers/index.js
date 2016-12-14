108501331b9650d7adb897c3ee0a3e5d2abd596f
Luis Herranz <luisherranz@gmail.com>
2016-12-14 18:19:37 +0100

    Fix assets loading (remote vs local).

--- client/packages/settings-dashboard-extension-worona/src/dashboard/reducers/index.js
+++ client/packages/settings-dashboard-extension-worona/src/dashboard/reducers/index.js
@@ -4,7 +4,10 @@ import { combineReducers } from 'redux';
 import * as deps from '../deps';
 
 const env = isDev ? 'dev' : 'prod';
-const mapPkg = pkg => ({ ...pkg, main: pkg.cdn && pkg.cdn.dashboard[env].main.file });
+const mapPkg = pkg => ({ ...pkg,
+  main: pkg.cdn && pkg.cdn.dashboard[env].main.file,
+  assets: pkg.cdn && pkg.cdn.dashboard[env].assets,
+});
 
 const createSetting = collection => combineReducers({
   collection: deps.reducerCreators.collectionCreator(collection),
