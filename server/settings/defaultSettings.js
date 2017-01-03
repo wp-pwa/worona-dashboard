c13717c27ee405b34f6dc09637b8d4001041edff
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2017-01-03 14:55:23 +0100

    Added default packages to server

--- server/settings/defaultSettings.js
+++ server/settings/defaultSettings.js
@@ -4,8 +4,24 @@ const defaults = [
     namespace: 'generalApp',
   },
   {
+    name: 'starter-app-theme-worona',
+    namespace: 'starterTheme',
+  },
+  {
     name: 'publish-native-app-extension-worona',
     namespace: 'publishNative',
+  },
+  {
+    name: 'extensions-manager-app-extension-worona',
+    namespace: 'extensionsManager',
+  },
+  {
+    name: 'general-fbia-extension-worona',
+    namespace: 'generalFbia',
+  },
+  {
+    name: 'general-amp-extension-worona',
+    namespace: 'generalAMP',
   },
 ];
 
