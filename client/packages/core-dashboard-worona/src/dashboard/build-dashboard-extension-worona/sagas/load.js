05847cb5283c02a99feafe73fc281389df30fa2e
Luis Herranz <luisherranz@gmail.com>
2017-01-04 18:43:08 +0100

    Use join strategy for waitFors.

--- client/packages/core-dashboard-worona/src/dashboard/build-dashboard-extension-worona/sagas/load.js
+++ client/packages/core-dashboard-worona/src/dashboard/build-dashboard-extension-worona/sagas/load.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-constant-condition */
 import { getSagas, getReducers, packageActivated, getDeps, waitForDeps } from 'worona-deps';
-import { put, call } from 'redux-saga/effects';
+import { put, call, join, fork } from 'redux-saga/effects';
 import { takeEvery } from 'redux-saga';
 import { addReducer, startSaga, reloadReducers, removeReducer, stopSaga } from '../store';
 import * as types from '../types';
@@ -26,11 +26,10 @@ export function* packageLoadSaga({ pkg }) {
     yield call(reloadReducers);
     yield call(loadSagas, pkg.name, pkg.namespace);
     if (pkg.assets) {
-      yield [
-        call(waitFor, pkg.name,
-          types.PACKAGE_ASSETS_LOAD_SUCCEED, types.PACKAGE_ASSETS_LOAD_FAILED),
-        put(actions.packageAssetsLoadRequested({ pkg })),
-      ];
+      const waitForAssets = yield fork(waitFor, pkg.name,
+        types.PACKAGE_ASSETS_LOAD_SUCCEED, types.PACKAGE_ASSETS_LOAD_FAILED);
+      yield put(actions.packageAssetsLoadRequested({ pkg }));
+      yield join(waitForAssets);
     }
     yield put(actions.packageLoadSucceed({ pkg }));
     yield call(packageActivated, pkg.name, 'dashboard');
