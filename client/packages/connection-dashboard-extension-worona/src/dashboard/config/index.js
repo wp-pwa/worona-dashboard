b085d40ce4896919f276fc7b76a8824fe172ed13
Luis Herranz <luisherranz@gmail.com>
2017-01-09 17:04:48 +0100

    Start using worona-asteroid.

--- client/packages/connection-dashboard-extension-worona/src/dashboard/config/index.js
+++ client/packages/connection-dashboard-extension-worona/src/dashboard/config/index.js
@@ -1,4 +1,4 @@
 import { isRemote } from 'worona-deps';
 
-export const url = isRemote ? 'wss://meteor.worona.io/websocket' : 'ws://localhost:3000/websocket';
+export const endpoint = isRemote ? 'wss://meteor.worona.io/websocket' : 'ws://localhost:3000/websocket';
 export const timeout = 10000;
