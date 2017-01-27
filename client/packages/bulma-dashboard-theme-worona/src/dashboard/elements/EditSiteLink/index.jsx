bc57138a9634ee8c826d2245f2d24f5a55effe60
Luis Herranz <luisherranz@gmail.com>
2017-01-27 10:46:28 +0100

    Add magin to Edit.

--- client/packages/bulma-dashboard-theme-worona/src/dashboard/elements/EditSiteLink/index.jsx
+++ client/packages/bulma-dashboard-theme-worona/src/dashboard/elements/EditSiteLink/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router';
+import styles from './style.css';
 
 const EditSiteLink = ({ id, color = 'white' }) => (
-  <Link to={`/edit-site/${id}`}>
+  <Link to={`/edit-site/${id}`} className={styles.editSite}>
     <span style={{ fontSize: '12px', fontWeight: 400, color }}>
       edit
     </span>
