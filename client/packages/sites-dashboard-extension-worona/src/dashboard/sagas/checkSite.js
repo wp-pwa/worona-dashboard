e8c1c7fa96a57166fad95b58045db5579725de49
Luis Herranz <luisherranz@gmail.com>
2017-02-09 18:03:58 +0100

    Change cors.

--- client/packages/sites-dashboard-extension-worona/src/dashboard/sagas/checkSite.js
+++ client/packages/sites-dashboard-extension-worona/src/dashboard/sagas/checkSite.js
@@ -11,7 +11,7 @@ import * as deps from '../deps';
 import * as libs from '../libs';
 import * as sagaHelpers from '../sagaHelpers';
 
-export const CORSAnywhere = 'https://cors.worona.io/cors/';
+export const CORSAnywhere = 'https://cors.worona.io/';
 export const woronaEndPoint = '/worona/v1/siteid';
 export const restRouteQuery = '?rest_route=';
 
