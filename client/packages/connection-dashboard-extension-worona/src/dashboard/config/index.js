5ac4715edfcefa5a3ac439ca18a154e5b3d7abc2
Luis Herranz <luisherranz@gmail.com>
2017-02-13 11:09:10 +0100

    Connect to premeteor from predashboard.

--- client/packages/connection-dashboard-extension-worona/src/dashboard/config/index.js
+++ client/packages/connection-dashboard-extension-worona/src/dashboard/config/index.js
@@ -1,4 +1,10 @@
 import { isRemote } from 'worona-deps';
 
-export const endpoint = isRemote ? 'wss://meteor.worona.io/websocket' : 'ws://localhost:3000/websocket';
+const remote = window.location.host === 'predashboard.worona.org'
+  ? 'wss://premeteor.worona.io/websocket'
+  : 'wss://meteor.worona.io/websocket';
+
+export const endpoint = isRemote
+  ? 'wss://meteor.worona.io/websocket'
+  : 'ws://localhost:3000/websocket';
 export const timeout = 10000;
