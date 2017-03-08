81133431956eb93dbfb7d5f05219ca1a9c3700c5
Luis Herranz <luisherranz@gmail.com>
2017-03-08 17:14:12 +0100

    Add redux-logger

--- client/packages/core-dashboard-worona/src/dashboard/build-dashboard-extension-worona/reducers/index.js
+++ client/packages/core-dashboard-worona/src/dashboard/build-dashboard-extension-worona/reducers/index.js
@@ -1,15 +1,9 @@
 /* eslint-disable no-console */
-import { isDev } from 'worona-deps';
 import { combineReducers } from 'redux';
 import assets from './assets';
 import packages from './packages';
 
-const log = (state, action) => {
-  if (isDev && !action.type.startsWith('@@redux')) console.log(action); return false;
-};
-
 export default () => combineReducers({
   packages,
   assets,
-  log,
 });
--- client/packages/core-dashboard-worona/src/dashboard/build-dashboard-extension-worona/store/index.js
+++ client/packages/core-dashboard-worona/src/dashboard/build-dashboard-extension-worona/store/index.js
@@ -1,38 +1,57 @@
 /* eslint-disable global-require */
 /* global window */
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import { isTest } from 'worona-deps';
-import { reduxReactRouter, routerStateReducer as router } from 'redux-router';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import build from '../reducers';
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import createSagaMiddleware from "redux-saga";
+import createLogger from "redux-logger";
+import { isTest, isDev } from "worona-deps";
+import { reduxReactRouter, routerStateReducer as router } from "redux-router";
+import { composeWithDevTools } from "redux-devtools-extension";
+import build from "../reducers";
 
 const sagaMiddleware = createSagaMiddleware();
+const loggerMiddleware = createLogger({
+  collapsed: true,
+  diff: true,
+  timestamp: false,
+});
+
+const middleware = isDev
+  ? [sagaMiddleware, loggerMiddleware]
+  : [sagaMiddleware];
 
 const reducers = { build: build(), router };
 const sagas = {};
 
-const composeEnhancers = composeWithDevTools({
-  serialize: false,
-});
+const composeEnhancers = composeWithDevTools({ serialize: false });
 
 export const store = createStore(
   combineReducers(reducers),
   composeEnhancers(
     reduxReactRouter({
-      createHistory: !isTest ? require('history').createHistory : require('history').createMemoryHistory,
+      createHistory: !isTest
+        ? require("history").createHistory
+        : require("history").createMemoryHistory,
     }),
-    applyMiddleware(sagaMiddleware),
+    applyMiddleware(...middleware)
   )
 );
 
 export default store;
 
 export const dispatch = action => store.dispatch(action);
-export const reloadReducers = () => store.replaceReducer(combineReducers(reducers));
-export const addReducer = (namespace, reducer) => { if (reducer) reducers[namespace] = reducer; };
-export const removeReducer = namespace => { if (reducers[namespace]) delete reducers[namespace]; };
-export const startSaga = (namespace, saga) => { sagas[namespace] = sagaMiddleware.run(saga); };
-export const stopSaga = (namespace) => { if (sagas[namespace]) sagas[namespace].cancel(); };
+export const reloadReducers = () =>
+  store.replaceReducer(combineReducers(reducers));
+export const addReducer = (namespace, reducer) => {
+  if (reducer) reducers[namespace] = reducer;
+};
+export const removeReducer = namespace => {
+  if (reducers[namespace]) delete reducers[namespace];
+};
+export const startSaga = (namespace, saga) => {
+  sagas[namespace] = sagaMiddleware.run(saga);
+};
+export const stopSaga = namespace => {
+  if (sagas[namespace]) sagas[namespace].cancel();
+};
 export const getState = store.getState.bind(store);
 export const history = store.history;
