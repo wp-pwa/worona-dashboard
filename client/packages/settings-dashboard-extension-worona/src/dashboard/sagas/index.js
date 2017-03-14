fdc261916da598434dd27a9bfa8cc33394fa4db3
Luis Herranz <luisherranz@gmail.com>
2017-03-14 20:15:02 +0100

    Client saga/reducer deactivations working.

--- client/packages/settings-dashboard-extension-worona/src/dashboard/sagas/index.js
+++ client/packages/settings-dashboard-extension-worona/src/dashboard/sagas/index.js
@@ -2,7 +2,7 @@
 import { isDev } from 'worona-deps';
 import { takeEvery } from 'redux-saga';
 import { find } from 'lodash';
-import { fork, call, select, put } from 'redux-saga/effects';
+import { fork, call, select, put, race, take } from 'redux-saga/effects';
 import * as deps from '../deps';
 import * as selectors from '../selectors';
 import * as actions from '../actions';
@@ -18,28 +18,44 @@ export function* activatePackage({ fields: { name } }) {
   yield put(deps.actions.packageActivationRequested({ pkg }));
 }
 
-export function* deactivatePackage({ id }) {
-  const pkgs = yield select(selectors.getPackageCollection);
-  debugger;
-  const pkg = find(pkgs, pkgObj => pkgObj.id === id);
-  yield put(deps.actions.packageDeactivationRequested({ pkg }));
+export function* deactivatePackages() {
+  let pkgs = [];
+  while (true) {
+    const { added, removed } = yield race({
+      added: take(
+        ({ type, collection, event }) =>
+        type === deps.types.COLLECTION_MODIFIED && collection === 'packages' && event === 'added'
+      ),
+      removed: take(
+        ({ type, collection, event }) =>
+        type === deps.types.COLLECTION_MODIFIED && collection === 'packages' && event === 'removed'
+      ),
+    });
+    if (added) {
+      pkgs = yield select(selectors.getPackageCollection);
+    } else {
+      const pkg = find(pkgs, item => item.id === removed.id);
+      yield put(deps.actions.packageDeactivationRequested({ pkg }));
+    }
+  }
 }
 
 export function* requestPackages() {
   yield [
-    call(deps.sagaHelpers.waitForReady, 'dashboard-settings-live', selectors.getSettingsLiveIsReady),
+    call(
+      deps.sagaHelpers.waitForReady,
+      'dashboard-settings-live',
+      selectors.getSettingsLiveIsReady
+    ),
     call(deps.sagaHelpers.waitForReady, 'packages', selectors.getPackageIsReady),
   ];
   const pkgs = yield select(selectors.getPackageCollection);
   yield pkgs.map(pkg => put(deps.actions.packageActivationRequested({ pkg })));
 
-  yield takeEvery(({ type, collection, event }) =>
-    type === deps.types.COLLECTION_MODIFIED && collection === 'packages' && event === 'added',
+  yield takeEvery(
+    ({ type, collection, event }) =>
+      type === deps.types.COLLECTION_MODIFIED && collection === 'packages' && event === 'added',
     activatePackage
-  );
-  yield takeEvery(({ type, collection, event }) =>
-    type === deps.types.COLLECTION_MODIFIED && collection === 'packages' && event === 'removed',
-    deactivatePackage
   );
 }
 
@@ -56,6 +72,7 @@ export default function* settingsagas() {
     fork(deps.sagaCreators.collectionWatcherCreator('packages')),
     fork(deps.sagaCreators.subscriptionWatcherCreator('packages', env)),
     fork(requestPackages),
+    fork(deactivatePackages),
     fork(saveSettings),
     fork(addSettings),
     takeEvery(deps.types.CREATE_SITE_SUCCEED, addDefaultSettings),
