26e9505fc08ebbbdf6eab8b53c461be436dee021
Luis Herranz <luisherranz@gmail.com>
2017-03-17 13:14:44 +0100

    Remove the need of namespaces in settings.

--- client/packages/settings-dashboard-extension-worona/src/dashboard/selectorCreators/index.js
+++ client/packages/settings-dashboard-extension-worona/src/dashboard/selectorCreators/index.js
@@ -5,15 +5,17 @@ import * as selectors from '../selectors';
 export const getSettings = packageNamespace =>
   createSelector(
     selectors.getSettingsLiveCollection,
+    deps.selectors.getActivatedPackages,
     deps.selectors.getSelectedSiteId,
-    (settings, siteId) =>
+    (settings, activated, siteId) =>
       settings.find(
-        item => item.woronaInfo.namespace === packageNamespace && item.woronaInfo.siteId === siteId,
+        item =>
+          item.woronaInfo.name === activated[packageNamespace] && item.woronaInfo.siteId === siteId,
       ) || { woronaInfo: {} }, // default to empty object til settings collections is loaded
   );
 
 export const getSetting = (packageNamespace, settingName, settingDefault = undefined) =>
-    createSelector(
-      getSettings(packageNamespace),
-      settings => settings && settings[settingName] || settingDefault,
-    );
+  createSelector(
+    getSettings(packageNamespace),
+    settings => (settings && settings[settingName]) || settingDefault,
+  );
