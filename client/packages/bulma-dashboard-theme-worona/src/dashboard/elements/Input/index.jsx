d448156ae9637b09fc2095325218819a5bea7f5f
David Arenas <darensr@gmail.com>
2017-07-04 10:13:34 +0200

    Added 'disabled' option for element Input.

--- client/packages/bulma-dashboard-theme-worona/src/dashboard/elements/Input/index.jsx
+++ client/packages/bulma-dashboard-theme-worona/src/dashboard/elements/Input/index.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import cx from 'classnames';
 import styles from './style.css';
 
-const Input = ({ input, meta: { touched, error }, size, label, icon, placeholder,
+const Input = ({ input, meta: { touched, error }, disabled, size, label, icon, placeholder,
    type, name }) =>
      <div className={styles.input}>
        {label && <label htmlFor={name} className="label">{label}</label>}
        <p className={cx('control', icon && 'has-icon')}>
          <input
            className={cx('input', error && touched
-            && 'is-danger', size && `is-${size}`)}
+            && 'is-danger', size && `is-${size}`, disabled && 'is-disabled')}
            {...input} placeholder={placeholder} type={type}
          />
          {icon && <i className={`fa fa-${icon}`} />}
@@ -18,13 +18,14 @@ const Input = ({ input, meta: { touched, error }, size, label, icon, placeholder
      </div>;
 
 Input.propTypes = {
-  input: React.PropTypes.object,
+  input: React.PropTypes.shape({}),
   name: React.PropTypes.string,
   label: React.PropTypes.string,
   meta: React.PropTypes.shape({
     error: React.PropTypes.string,
     touched: React.PropTypes.bool,
   }),
+  disabled: React.PropTypes.bool,
   size: React.PropTypes.string,
   icon: React.PropTypes.string,
   placeholder: React.PropTypes.string,
